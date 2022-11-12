66dcac9e0eb3f61e0a78366954a6c59dabd0eeb9
Bekbolsun Akchalov <bekbolsun.akchalov@gmail.com>
2022-11-12 19:13:30 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import javax.xml.namespace.QName;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println(a||b);
+
+
+
+
+    }
+
+
+}
